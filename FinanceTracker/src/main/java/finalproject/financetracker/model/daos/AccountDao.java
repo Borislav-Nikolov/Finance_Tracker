319b069f87eef76360e55300e9ae23522f2111d1
Stanislav Kolev <spite@abv.bg>
2019-02-26 00:13:24 +0200

    accoundDao prepStatement/resSet.close()->finally{}

--- FinanceTracker/src/main/java/finalproject/financetracker/model/daos/AccountDao.java
+++ FinanceTracker/src/main/java/finalproject/financetracker/model/daos/AccountDao.java
@@ -47,30 +47,43 @@ public class AccountDao extends AbstractDao {
     }
 
     public void updateAcc(Account acc) throws SQLException {
-        String sql = "UPDATE final_project.accounts SET account_name = ?, amount = ? WHERE account_id = ?;";
-        PreparedStatement ps = mySQLCon.prepareStatement(sql,Statement.RETURN_GENERATED_KEYS);
-        ps.setString(1, acc.getAccountName());
-        ps.setDouble(2, acc.getAmount());
-        ps.setLong(3, acc.getAccountId());
-        ps.executeUpdate();
-        closeStatement(ps);
+        PreparedStatement ps = null;
+        try {
+            String sql = "UPDATE final_project.accounts SET account_name = ?, amount = ? WHERE account_id = ?;";
+            ps = mySQLCon.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, acc.getAccountName());
+            ps.setDouble(2, acc.getAmount());
+            ps.setLong(3, acc.getAccountId());
+            ps.executeUpdate();
+        }
+        finally {
+            closeStatement(ps);
+        }
+
     }
 
     public int getAllCount(long userId) throws SQLException {
-        String sql = "SELECT COUNT(*) AS count " +
-                "FROM final_project.accounts AS a " +
-                "WHERE a.user_id  = ?;";
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        int result;
+        try {
+            String sql = "SELECT COUNT(*) AS count " +
+                    "FROM final_project.accounts AS a " +
+                    "WHERE a.user_id  = ?;";
 
-        PreparedStatement ps = mySQLCon.prepareStatement(sql);
-        ps.setLong(1, userId);
-        ResultSet rs = ps.executeQuery();
-        int result =-1;
+            ps = mySQLCon.prepareStatement(sql);
+            ps.setLong(1, userId);
+            rs = ps.executeQuery();
+            result = -1;
 
-        if (rs.next()) {
-            result = rs.getInt(1);
+            if (rs.next()) {
+                result = rs.getInt(1);
+            }
         }
-        closeResultSet(rs);
-        closeStatement(ps);
+        finally {
+            closeResultSet(rs);
+            closeStatement(ps);
+        }
         if (result<0) throw new SQLException("error retrieving ResultSet");
         return result;
     }
@@ -88,49 +101,62 @@ public class AccountDao extends AbstractDao {
     }
 
     private Account[] getAllWhere(SQLColumnName param, SQLCompareOperator operator, long idColumnValueLong, int limit, int offset, SQLOderBy order) throws SQLException {
-        System.out.println(order);
-        String sql =
-                "SELECT a.account_id, a.account_name, a.amount, a.user_id " +
-                        "FROM final_project.accounts AS a " +
-                        "WHERE a." + param.toString() + " " + operator.getValue() + " ? ORDER BY a.account_name " + order + " LIMIT ? OFFSET ?;";
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        ArrayList<Account> arr;
+        try {
+            System.out.println(order);
+            String sql =
+                    "SELECT a.account_id, a.account_name, a.amount, a.user_id " +
+                            "FROM final_project.accounts AS a " +
+                            "WHERE a." + param.toString() + " " + operator.getValue() + " ? ORDER BY a.account_name " + order + " LIMIT ? OFFSET ?;";
 
-        PreparedStatement ps = mySQLCon.prepareStatement(sql);
+            ps = mySQLCon.prepareStatement(sql);
 
-        ps.setLong(1, idColumnValueLong);
-        ps.setInt(2, limit);
-        ps.setInt(3, offset);
-        ResultSet rs = ps.executeQuery();
-        ArrayList<Account> arr = new ArrayList<>();
+            ps.setLong(1, idColumnValueLong);
+            ps.setInt(2, limit);
+            ps.setInt(3, offset);
+            rs = ps.executeQuery();
+            arr = new ArrayList<>();
 
-        while (rs.next()) {
-            arr.add(new Account(
-                    rs.getLong("account_id"),
-                    rs.getString("account_name"),
-                    rs.getDouble("amount"),
-                    rs.getLong("user_id"))
-            );
+            while (rs.next()) {
+                arr.add(new Account(
+                        rs.getLong("account_id"),
+                        rs.getString("account_name"),
+                        rs.getDouble("amount"),
+                        rs.getLong("user_id"))
+                );
+            }
         }
-        closeStatement(ps);
-        closeResultSet(rs);
+        finally {
+            closeStatement(ps);
+            closeResultSet(rs);
+        }
         return arr.toArray(new Account[0]);
     }
 
     public long addAcc(Account acc) throws SQLException {
-        String sql = "INSERT INTO final_project.accounts(account_name, user_id, amount) VALUES (?, ?, ?);";
-        PreparedStatement ps = mySQLCon.prepareStatement(sql,Statement.RETURN_GENERATED_KEYS);
-        ps.setString(1, acc.getAccountName());
-        ps.setLong(2, acc.getUserId());
-        ps.setDouble(3, acc.getAmount());
-        ps.executeUpdate();
-        ResultSet rs = ps.getGeneratedKeys();
-        long accId = -1;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        long accId;
+        try {
+            String sql = "INSERT INTO final_project.accounts(account_name, user_id, amount) VALUES (?, ?, ?);";
+            ps = mySQLCon.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, acc.getAccountName());
+            ps.setLong(2, acc.getUserId());
+            ps.setDouble(3, acc.getAmount());
+            ps.executeUpdate();
+            rs = ps.getGeneratedKeys();
+            accId = -1;
 
-        if (rs.next()){
-            accId = rs.getLong(1);
+            if (rs.next()){
+                accId = rs.getLong(1);
+            }
         }
-        closeResultSet(rs);
-        closeStatement(ps);
-
+        finally {
+            closeResultSet(rs);
+            closeStatement(ps);
+        }
         if (accId<=0) {
             throw new SQLException("error retrieving data from data base");
         }
@@ -138,37 +164,47 @@ public class AccountDao extends AbstractDao {
     }
 
     public int deleteAcc(SQLColumnName param, SQLCompareOperator operator, long idColumn) throws SQLException {
-        String sql =
-                "DELETE a FROM final_project.accounts AS a " +
-                        "WHERE a." + param.toString() + " " + operator.getValue() + " ?;";
+        PreparedStatement ps = null;
+        int affectedRows;
+        try {
+            String sql =
+                    "DELETE a FROM final_project.accounts AS a " +
+                            "WHERE a." + param.toString() + " " + operator.getValue() + " ?;";
 
-        PreparedStatement ps = mySQLCon.prepareStatement(sql);
-        ps.setLong(1, idColumn);
-        int affectedRows = ps.executeUpdate();
-        closeStatement(ps);
+            ps = mySQLCon.prepareStatement(sql);
+            ps.setLong(1, idColumn);
+            affectedRows = ps.executeUpdate();
+        } finally {
+            closeStatement(ps);
+        }
         return affectedRows;
     }
 
     public Account getById(long id) throws SQLException, NotFoundException {
-        String sql =
-                "SELECT a.account_id, a.account_name, a.amount, a.user_id " +
-                        "FROM final_project.accounts AS a " +
-                        "WHERE a.account_id = ?;";
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        try {
+            String sql =
+                    "SELECT a.account_id, a.account_name, a.amount, a.user_id " +
+                            "FROM final_project.accounts AS a " +
+                            "WHERE a.account_id = ?;";
 
-        PreparedStatement ps = mySQLCon.prepareStatement(sql);
-        ps.setLong(1, id);
-        ResultSet rs = ps.executeQuery();
+            ps = mySQLCon.prepareStatement(sql);
+            ps.setLong(1, id);
+            rs = ps.executeQuery();
 
-        if (rs.next()) {
-            return new Account(
-                    rs.getLong("account_id"),
-                    rs.getString("account_name"),
-                    rs.getDouble("amount"),
-                    rs.getLong("user_id"));
+            if (rs.next()) {
+                return new Account(
+                        rs.getLong("account_id"),
+                        rs.getString("account_name"),
+                        rs.getDouble("amount"),
+                        rs.getLong("user_id"));
+            }
         }
-        closeStatement(ps);
-        closeResultSet(rs);
+        finally {
+            closeStatement(ps);
+            closeResultSet(rs);
+        }
         throw new NotFoundException("account " + id + " not found");
     }
-
 }
